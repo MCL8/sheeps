2b42a43c1f83dfed2197815521ca9c541786608a
Igor F <ig.freid@gmail.com>
2020-07-07 02:05:35 +0600

    migrations added

---
+++ database/migrations/2020_07_04_160210_create_sheep_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSheepTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sheep', function (Blueprint $table) {
+            $table->id();
+            $table->integer('corral')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sheep');
+    }
+}
---
+++ database/migrations/2020_07_06_172044_create_reports_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('day');
+            $table->integer('sheeps_total');
+            $table->integer('sheeps_alive');
+            $table->integer('sheeps_dead');
+            $table->integer('corral_max');
+            $table->integer('corral_min');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
